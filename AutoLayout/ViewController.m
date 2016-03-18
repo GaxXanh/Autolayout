f0c609ece7ceb76d5bf13a78a77a3695652f99dc
Pham Anh <anhapo11@gmail.com>
2016-03-19 02:08:00 +0700

    First commit

---
+++ AutoLayout/ViewController.m
@@ -0,0 +1,36 @@
+//
+//  ViewController.m
+//  AutoLayout
+//
+//  Created by Pham Anh on 3/15/16.
+//  Copyright © 2016 com.gaxxanh. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "Masonry.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UISearchBar *searchBar;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [self.searchBar mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view.mas_top).with.offset(20);
+        make.left.equalTo(self.view.mas_left).with.offset(20);
+        make.right.equalTo(self.view.mas_right).with.offset(20);
+    }];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
